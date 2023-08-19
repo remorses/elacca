f4b7d623bd55425129b4d329a26cd441259c4a14
remorses <beats.by.morse@gmail.com>
2023-08-19 13:18:35 +0200

    rerun on app

--- example-app/plugin-outputs/server-_app.tsx
+++ example-app/plugin-outputs/server-_app.tsx
@@ -2,7 +2,6 @@
 
 import { ChakraProvider } from '@chakra-ui/react';
 import './styles.css';
-import type { AppProps } from 'next/app';
 function DefaultExportRenamedByElacca() {
   return null;
 }
--- example-app/plugin-outputs/server-index.tsx
+++ example-app/plugin-outputs/server-index.tsx
@@ -1,7 +1,5 @@
 'skip ssr';
 
-import { Checkbox, FormControl, FormLabel, HStack, PinInput, PinInputField, Select, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Switch, Textarea } from '@chakra-ui/react';
-import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
 import { createContext } from 'react';
 export function getServerSideProps() {
